f1e25b9b2370ecc96c147db028392bff0714c5a5
amankumar89 <amankumaroo784@gmail.com>
2026-01-11 11:21:12 +0530

    Add Greeting component to display random greetings and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import Greeting from "./use-effect/Greeting";
 import Colorizer from "./use-state/Colorizer";
 import Counter from "./use-state/Counter";
 
 const App = () => {
   return (
     <div className="App px-8">
-      <Counter />
-      <Colorizer />
+      {/* use-state */}
+
+      {/* <Counter />
+      <Colorizer /> */}
+
+      {/* use-effect */}
+      {/* <Greeting max={8} /> */}
     </div>
   );
 };
