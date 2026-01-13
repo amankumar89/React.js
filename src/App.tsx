d60e73c688cc0ecc53810625b08b73df3456c871
amankumar89 <amankumaroo784@gmail.com>
2026-01-13 11:17:29 +0530

    Add CounterNew component using useRef for counting and update App component to include it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import FindUser from "./use-effect/FindUser";
 import Greeting from "./use-effect/Greeting";
+import CounterNew from "./use-ref/CounterNew";
 import Colorizer from "./use-state/Colorizer";
 import Counter from "./use-state/Counter";
 
 const App = () => {
   return (
     <div className="App px-8">
+      {/* use-ref */}
+      <CounterNew />
       {/* use-effect */}
       <FindUser />
       <Greeting max={8} />
