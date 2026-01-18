f9a305fec18faccffbbc9400d682f7f0e04078c0
amankumar89 <amankumaroo784@gmail.com>
2026-01-18 18:57:20 +0530

    Add controlled and uncontrolled component patterns to App
    - Introduced Counter, AutoFocusInput, and CounterWithRef components.
    - Updated App component to include new design patterns for better demonstration of controlled and uncontrolled components.

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ import Focus from "./use-ref/Focus";
 import Colorizer from "./use-state/Colorizer";
 import Counter from "./use-state/Counter";
 import UserProfileContainer from "./design-patterns/01-container-presenter/with-pattern/components/profile/UserProfileContainer";
+import CounterDesignPattern from "./design-patterns/02-controlled-uncontrolled-component/state-ref/components/Counter";
+import AutoFocusInput from "./design-patterns/02-controlled-uncontrolled-component/state-ref/components/AutoFocusInput";
+import CounterWithRef from "./design-patterns/02-controlled-uncontrolled-component/state-ref/components/CounterWithRef";
 
 const App = () => {
   return (
     <div className="App px-8">
       {/* react-design-patterns */}
+      <div className="flex flex-col gap-6">
+        <CounterDesignPattern />
+        <AutoFocusInput />
+        <CounterWithRef />
+      </div>
       <UserProfileContainer userId={1} />
       {/* hoc */}
       <MovieListHoc />
